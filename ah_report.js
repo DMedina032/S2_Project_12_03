10ba78bd8a96aa95c8ae1b0ee3c4247008f56f32
Unknown <dmedina032@west-mec.org>
2019-03-05 17:19:52 -0700

    Final commit/ submit for review
    I fixed my code and added in some arrays. I was then able to write my comments.

--- ah_report.js
+++ ah_report.js
@@ -27,25 +27,24 @@
       information for the donor
 
 */
-var donationTotal = toLocalString(0);
+// I declared the donationTotal by putting a var attribute and set the value to 0 as the starting point. I set the anys of donors and did a foreach that belongs to donors by putting the ".".
+var donationTotal = 0;
 donors.forEach(calcSum);
-var summaryTable = "<table><tr><th>Donors</th><td>" +donors.length+"</td></tr><tr><th>Total Donations</th><td>"+ "$"+donationTotal+"</td></th></table>";
+// By using the var attribute, i was then able the declare the varible summaryTable and gave it the value of the text string for the table concatanating it for the donors.length along with conatinating it to the table data and table head string for the donors.
+var summaryTable = "<table><tr><th>Donors</th><td>" + donors.length + "</td></tr><tr><th>TotalDonations</th><td>$" + donationTotal.toLocaleString() + "</td></th></table>";
+// By using the document.getElementbyId js was then able to get the id of "donation summary" form the index.html and used the innerHTML and gave that the value of the varible summary table.
 document.getElementById("donationSummary").innerHTML = summaryTable;
-var donorTable = "<table>
-<caption>Major Donors</caption>
-<tr>
-<th>Donation</th> <th>Donor ID</th>
-<th>Date</th><th>Name</th><th>Address</th>
-<th>Phone</th> <th>E-mail</th>
-</tr>";
+// I declared the majorDonors varible by using the var attribute and gave it the value donors.filter along with perameters.
+var majorDonors = donors.filter(findMajorDonors);
+majorDonors.sort(donorSortDescending);
+// By using the var attribute I was then able to declare the varible of donor table and set the value to a string for the table.
+var donorTable = "<table><caption>Major Donors</caption><tr><th>Donation</th><th>Donor ID</th><th>Date</th><th>Name</th><th>Address</th><th>Phone</th><th>E-mail</th></tr>";
+
+majorDonors.forEach(writeDonorRow);
+donorTable += "</table>";
+document.getElementById("donorTable").innerHTML = donorTable;
 
 
-function calcSum(donorAmt) {
-donationTotal += donorAmt[9];
-}
-function findMajorDonors(donorAmt) {
-return donorAmt[9] >= 1000;
-}
 
 
 
@@ -54,25 +53,25 @@ return donorAmt[9] >= 1000;
 
 
 function calcSum(donorAmt) {
-   donationTotal += donorAmt[9];
+      donationTotal += donorAmt[9];
 }
 
 function findMajorDonors(donorAmt) {
-   return donorAmt[9] >= 1000;
+      return donorAmt[9] >= 1000;
 }
 
 function donorSortDescending(a, b) {
-   return b[9] - a[9];
+      return b[9] - a[9];
 }
 
 function writeDonorRow(value) {
-   donorTable += "<tr>";
-   donorTable += "<td>$" + value[9].toLocaleString() + "</td>";
-   donorTable += "<td>" + value[0] + "</td>";
-   donorTable += "<td>" + value[10] + "</td>";
-   donorTable += "<td>" + value[2] + ", " + value[1] + "</td>";
-   donorTable += "<td>" + value[3] + "<br />" + value[4] + ", " + value[5] + " " + value[6]  + "</td>";
-   donorTable += "<td>" + value[7] + "</td>";
-   donorTable += "<td>" + value[8] + "</td>";
-   donorTable += "</tr>";
+      donorTable += "<tr>";
+      donorTable += "<td>$" + value[9].toLocaleString() + "</td>";
+      donorTable += "<td>" + value[0] + "</td>";
+      donorTable += "<td>" + value[10] + "</td>";
+      donorTable += "<td>" + value[2] + ", " + value[1] + "</td>";
+      donorTable += "<td>" + value[3] + "<br />" + value[4] + ", " + value[5] + " " + value[6] + "</td>";
+      donorTable += "<td>" + value[7] + "</td>";
+      donorTable += "<td>" + value[8] + "</td>";
+      donorTable += "</tr>";
 }
